dadc8a1bab8eca7db174fb0b79f55c4beb6fda6c
vaibhavbarhate <vaibhavbarhate4@gmail.com>
2024-05-20 17:34:58 +0530

    "Updated HTML, CSS, and JS files: added AOS animation library, modified header and footer sections, updated styles and scripts, and added preloader functionality."

--- js/script.js
+++ js/script.js
@@ -21,3 +21,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 400);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
